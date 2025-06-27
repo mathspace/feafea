c16038279c61fb27ff56cff8fe2ea637cd5db4bd
Mansour Behabadi <mbehabadi@mathspace.com.au>
2025-06-27 17:25:30 +1000

    Add test case for attribute set filtering

--- tests/test_evaluator.py
+++ tests/test_evaluator.py
@@ -14,7 +14,7 @@ class TestEvaluator(unittest.TestCase):
                         "default": False,
                     },
                     "b": {
-                        "variants": [1, 2, 3],
+                        "variants": [1, 2, 3, 4],
                         "default": 1,
                     },
                 },
@@ -29,6 +29,12 @@ class TestEvaluator(unittest.TestCase):
                         "filter": "attr:some_other_attr = 6",
                         "variants": {
                             "b": 3,
+                        },
+                    },
+                    "r2": {
+                        "filter": "88 in attr:some_set_attr",
+                        "variants": {
+                            "b": 4,
                         },
                     },
                 },
@@ -57,18 +63,18 @@ class TestEvaluator(unittest.TestCase):
             evaluator.evaluate(self._valid_config, "a", cast(str, 1))
 
         invalid_attributes = [
-            "non_dict",
-            set([1, 2]),
-            [],
-            {99: 11},  # non str key
-            {"b": (1, 2, 3)},  # tuple value
-            {"c": {"a": 1}},  # dict value
-            {"d": {(1, 2), (3, 4)}},  # set of tuples
-            {"e": {None, 1}},  # invalid set element
-            {"e": {2, "3"}},  # different set element types
+            ("non_dict", "must be a dict"),
+            (set([1, 2]), "must be a dict"),
+            ([], "must be a dict"),
+            ({99: 11}, "attribute key must be a string"), # int key
+            ({"b": (1, 2, 3)}, "attribute value must be"), # tuple
+            ({"c": {"a": 1}}, "attribute value must be"), # dict key
+            ({"d": {(1, 2), (3, 4)}}, "set values must be"), # set with tuple elements
+            ({"e": {None, 1}}, "set values must be"), # set with None
+            ({"e": {2, "3"}}, "set values must be of the same type"), # set with mixed int and string types
         ]
-        for attr in invalid_attributes:
-            with self.assertRaises(TypeError):
+        for attr, err in invalid_attributes:
+            with self.assertRaisesRegex(TypeError, err, msg=attr):
                 evaluator.evaluate(self._valid_config, "a", "", attr)
 
     def test_additional_attribute_validation_errors(self):
@@ -122,6 +128,7 @@ class TestEvaluator(unittest.TestCase):
         self.assertEqual(evaluator.evaluate(cfg, "b", ""), 1)
         self.assertEqual(evaluator.evaluate(cfg, "a", "", {"some_attr": 5, "some_other_attr": 6}), True)
         self.assertEqual(evaluator.evaluate(cfg, "b", "", {"some_attr": 5, "some_other_attr": 6}), 3)
+        self.assertEqual(evaluator.evaluate(cfg, "b", "", {"some_set_attr": {100, 88}}), 4)
 
     def test_record_evaluations(self):
         evaluator = Evaluator(record_evaluations=True)
